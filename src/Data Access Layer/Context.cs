8f1789b137a9d43eb09c4e1741f436546c6576c0
Tristan <47876307+tristanl2170@users.noreply.github.com>
2020-12-08 10:01:24 -0500

    Add files via upload

---
+++ src/Data Access Layer/Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using PatientPortalApplication.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace PatientPortalApplication.DAL
+{
+    public class Context: DbContext
+    {
+        public Context() : base("Context")
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+
+        public DbSet<Prescrips> Prescrips { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+    
+
+}
---
+++ src/Data Access Layer/Initializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PatientPortalApplication.Models;
+
+
+
+namespace PatientPortalApplication.DAL
+{
+    public class Initializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<Context>
+    {
+
+        protected override void Seed(Context context)
+        {
+
+
+           
+            context.SaveChanges();
+        }
+    }
+
+
+}
