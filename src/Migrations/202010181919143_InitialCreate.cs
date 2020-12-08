7aac59cc054d33853bf075a5cf928c6dd8497bfc
Tristan <47876307+tristanl2170@users.noreply.github.com>
2020-12-08 09:51:44 -0500

    Add files via upload

---
+++ src/Migrations/202010181919143_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿namespace PatientPortalApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Data.Entity.Migrations.Builders;
+
+
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.comments",
+                c => new
+                    {
+                        commentId = c.Int(nullable: false, identity: true),
+                        comment = c.String(),
+                        date = c.DateTime(nullable: false),
+                        patientid = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.commentId)
+                .ForeignKey("dbo.patient", t => t.patientid, cascadeDelete: true)
+                .Index(t => t.patientid);
+            
+            CreateTable(
+                "dbo.patient",
+                c => new
+                    {
+                        patientId = c.Int(nullable: false, identity: true),
+                        first_name = c.String(),
+                        last_name = c.String(),
+                        date_admitted = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.patientId);
+            
+            CreateTable(
+                "dbo.prescriptions",
+                c => new
+                    {
+                        prescripId = c.Int(nullable: false, identity: true),
+                        prescriptions = c.String(),
+                        patientid = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.prescripId)
+                .ForeignKey("dbo.patient", t => t.patientid, cascadeDelete: true)
+                .Index(t => t.patientid);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.comments", "patientid", "dbo.patient");
+            DropForeignKey("dbo.prescriptions", "patientid", "dbo.patientr");
+            DropIndex("dbo.prescriptions", new[] { "patientid" });
+            DropIndex("dbo.comments", new[] { "patientid" });
+            DropTable("dbo.prescriptions");
+            DropTable("dbo.patient");
+            DropTable("dbo.comments");
+        }
+    }
+}
---
+++ src/Migrations/202011211715511_TestMigration.cs
@@ -0,0 +1,47 @@
+﻿namespace PatientPortalApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TestMigration : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.patient", "Patient_patientId", "dbo.patient");
+            DropIndex("dbo.comments", new[] { "patientid" });
+            DropIndex("dbo.patient", new[] { "Patient_patientId" });
+            DropIndex("dbo.prescrips", new[] { "patientid" });
+            RenameColumn(table: "dbo.comments", name: "commentId", newName: "Comment Id");
+            RenameColumn(table: "dbo.comments", name: "date", newName: "Comment Date");
+            RenameColumn(table: "dbo.comments", name: "comment", newName: "Comment");
+            RenameColumn(table: "dbo.patient", name: "patientId", newName: "Patient Id");
+            RenameColumn(table: "dbo.patient", name: "first_name", newName: "First Name");
+            RenameColumn(table: "dbo.patient", name: "last_name", newName: "Last Name");
+            RenameColumn(table: "dbo.patient", name: "date_admitted", newName: "Date Admitted");
+            RenameColumn(table: "dbo.prescrips", name: "prescripId", newName: "Prescription Id");
+            RenameColumn(table: "dbo.prescrips", name: "prescriptions", newName: "Prescription");
+            CreateIndex("dbo.comments", "patientId");
+            CreateIndex("dbo.prescrips", "patientId");
+            
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.patient", "Patient_patientId", c => c.Int());
+            DropIndex("dbo.prescrips", new[] { "patientId" });
+            DropIndex("dbo.comments", new[] { "patientId" });
+            RenameColumn(table: "dbo.prescrips", name: "Prescription", newName: "prescriptions");
+            RenameColumn(table: "dbo.prescrips", name: "Prescription Id", newName: "prescripId");
+            RenameColumn(table: "dbo.patient", name: "Date Admitted", newName: "date_admitted");
+            RenameColumn(table: "dbo.patient", name: "Last Name", newName: "last_name");
+            RenameColumn(table: "dbo.patient", name: "First Name", newName: "first_name");
+            RenameColumn(table: "dbo.patient", name: "Patient Id", newName: "patientId");
+            RenameColumn(table: "dbo.comments", name: "Comment Date", newName: "date");
+            RenameColumn(table: "dbo.comments", name: "Comment Id", newName: "commentId");
+            CreateIndex("dbo.prescrips", "patientid");
+            CreateIndex("dbo.patient", "Patient_patientId");
+            CreateIndex("dbo.comments", "patientid");
+            AddForeignKey("dbo.patient", "Patient_patientId", "dbo.patient", "patientId");
+        }
+    }
+}
