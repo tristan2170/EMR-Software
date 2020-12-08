9d4061ad838710a46a34a47b717b980126282171
Tristan <47876307+tristanl2170@users.noreply.github.com>
2020-12-08 09:34:21 -0500

    Add files via upload

---
+++ src/Models/Comments.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PatientPortalApplication.Models
+{
+
+    [Table("comments")]
+    public class Comments
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Key]
+
+        [Column("Comment Id")]
+        public int commentId { get; set; }
+
+        [Column("Comment")]
+        public string comment { get; set; }
+
+        [Column("Comment Date", TypeName ="DateTime")]
+        public DateTime date { get; set; }
+
+
+        [ForeignKey("Patient")]
+        public int patientId { get; set; }
+        public Patient Patient { get; set; }
+    }
+
+
+}
---
+++ src/Models/Patient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace PatientPortalApplication.Models
+{
+    [Table("patient")]
+    public class Patient
+    {
+        
+            [Key]
+            [Column("Patient Id")]
+            public int patientId { get; set; }
+
+            [Column("First Name")]
+            public string first_name { get; set; }
+
+            [Column("Last Name")]
+            public string last_name { get; set; }
+            
+            [Column("Date Admitted", TypeName = "DateTime")]
+            public DateTime date_admitted { get; set; }
+
+
+            
+            public virtual ICollection<Comments> Comments { get; set; } 
+            public virtual ICollection<Prescrips> Prescrips { get; set; }
+            public virtual ICollection<Patient> Patients { get; set; }
+        
+    }
+}
