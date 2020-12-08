f08c21ee59a2f34a9e25d8d4959314342008eb41
Tristan <47876307+tristanl2170@users.noreply.github.com>
2020-12-08 09:31:17 -0500

    Add files via upload

---
+++ src/Prescrips.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using PatientPortalApplication.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PatientPortalApplication.Models
+{
+    [Table("prescrips")]
+    public class Prescrips
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Key]
+        [Column("Prescription Id")]
+        public int prescripId { get; set; }
+
+        [Column("Prescription")]
+        public string prescriptions { get; set; }
+
+
+        [ForeignKey("Patient")]
+        public int patientId { get; set; }                
+        public Patient Patient { get; set; }
+
+    }
+
+
+
+}
