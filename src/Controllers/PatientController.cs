7293c0af1cb3b6168486564cf9cee46f5939b3c2
Tristan <47876307+tristanl2170@users.noreply.github.com>
2020-12-08 09:44:31 -0500

    Add files via upload

---
+++ src/Controllers/PatientController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PatientPortalApplication.DAL;
+using PatientPortalApplication.Models;
+using PatientPortalApplication.ViewModel;
+
+
+namespace PatientPortalApplication.Controllers
+{
+    public class PatientController : Controller
+    {
+        private Context db = new Context();
+        
+
+        // GET: Patient
+        public ActionResult Index()
+        {
+            return View(db.Patients.ToList());
+        }
+
+        // GET: Patient/Details/5
+        public ActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients
+                .Include(a => a.Comments)
+                .Include(b => b.Prescrips)
+                .Include(c => c.Patients)
+                .Where(a => a.patientId == id)
+                .Where(b => b.patientId == id)
+                .Where(c => c.patientId == id)
+                .SingleOrDefault();
+                
+                
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+
+            var view = new DetailView
+            {
+                Comments = patient.Comments.ToList(),
+                Prescrips = patient.Prescrips.ToList(),
+                Patients = patient.Patients.ToList()
+            };
+        
+            return View(view);
+        }
+
+       
+
+        // GET: Patient/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patient/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,first_name,last_name,date_admitted")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patients.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(patient);
+        }
+
+        // GET: Patient/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patient/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,first_name,last_name,date_admitted")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+
+        // GET: Patient/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patient/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Patient patient = db.Patients.Find(id);
+            db.Patients.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
