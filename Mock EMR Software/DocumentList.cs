c45cf2e3aa66dd17abceb14490646d9777e7d62c
Tristan <tristanlinnenqhst@gmail.com>
2024-01-31 20:00:31 -0500

    Added JSON files with orderable and document names with readers

---
+++ Mock EMR Software/DocumentList.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Reflection.Metadata;
+
+namespace Mock_EMR_Software
+{
+    public class DocumentList
+    {
+        public static void main()
+        {
+            List<string> document_list = new List<string>();
+
+            string filePath = "Documents.json";
+            string jsonContent = File.ReadAllText(filePath);
+
+            JArray jsonArray = JArray.Parse(jsonContent);
+
+            foreach (JObject obj in jsonArray)
+            {
+                string doc_name = obj["name"].ToString();
+                document_list.Add(doc_name);
+
+            }
+        }
+    }
+}
---
+++ Mock EMR Software/OrderList.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Mock_EMR_Software
+{
+    public class OrderList
+    {
+        public static void main()
+        {
+            List<string> document_list = new List<string>();
+
+            string filePath = "Orders.json";
+            string jsonContent = File.ReadAllText(filePath);
+
+            JObject jsonObject = JObject.Parse(jsonContent);
+
+            JArray drugsArray = (JArray)jsonObject["drugs"];
+
+            List<string> drug_list = drugsArray.ToObject<List<string>>();
+
+           
+        }
+    }
+   
+}
+
